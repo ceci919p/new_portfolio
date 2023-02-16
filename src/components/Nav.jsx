0bcc5e801ac0d592745ad870f40e925bd3a23424
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2023-02-16 15:53:06 +0100

    file structure

---
+++ src/components/Nav.jsx
@@ -0,0 +1,7 @@
+export default function Nav() {
+  return (
+    <div>
+      <p>This is a header</p>
+    </div>
+  );
+}
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,17 @@
 import "@/styles/globals.scss";
+/*put global components here!*/
+
+import Nav from "./../components/Nav";
+import Footer from "./../components/Footer";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div className=" text-black">
+      <Nav />
+
+      <Component {...pageProps} />
+
+      <Footer />
+    </div>
+  );
 }
