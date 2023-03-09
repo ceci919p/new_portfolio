72f93e57470e4611a2ca5a7a163bb7b7c29b77b1
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2023-03-09 17:17:46 +0100

    splash added to playground

--- src/pages/playground.js
+++ src/pages/playground.js
@@ -1,11 +1,9 @@
-/* playground page */
+import Splash from "@/components/playground_sections/Splash";
 
 export default function Playground() {
   return (
     <main>
-      <section>
-        <p>welcome the playground</p>
-      </section>
+      <Splash></Splash>
     </main>
   );
 }
