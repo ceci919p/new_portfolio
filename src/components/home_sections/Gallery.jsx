18894170203e0359055bc9803af48c9fecf18da2
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2023-03-10 13:28:05 +0100

    hover ani added to gallery

--- src/components/home_sections/Gallery.jsx
+++ src/components/home_sections/Gallery.jsx
@@ -1,19 +1,27 @@
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Gallery({ thumbnail, slug }) {
+export default function Gallery({ thumbnail, slug, semester, title }) {
   const img = thumbnail.responsiveImage;
 
   return (
     <Link href={`/projects/${slug}`} passHref>
-      <Image
-        className="thumbnail"
-        src={img.src}
-        alt={img.alt}
-        width={img.width}
-        height={img.height}
-        sizes={img.sizes}
-      ></Image>
+      <article className="container">
+        <Image
+          className="thumbnail"
+          src={img.src}
+          alt={img.alt}
+          width={img.width}
+          height={img.height}
+          sizes={img.sizes}
+        ></Image>
+        <div className="overlay">
+          <div id="display_text">
+            <h2>{title}</h2>
+            <p>{semester}</p>
+          </div>
+        </div>
+      </article>
     </Link>
   );
 }
