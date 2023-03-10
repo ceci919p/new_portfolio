47068a952236122f1daa25e0936296403be39976
Cecilie Jasmin Jørgensen <ceci919p@stud.kea.dk>
2023-03-10 14:07:06 +0100

    responsive footer

--- src/components/home_sections/About.jsx
+++ src/components/home_sections/About.jsx
@@ -3,14 +3,11 @@ export default function About() {
     <section id="about">
       <p>
         Hello World, I’m Cecilie Jasmin, a digital designer. <br></br>
-        in 2020 I quit my studies in Architectural Technology and Construction
-        Management when I found myself more interested in the design of the
-        buildings. <br></br>
-        The following year I took the chance and began studying multimedia
-        design. This was where I found my passion in UX/UI and surprisingly,
-        Frontend Development as well. <br></br>
-        Since then I have gained 2 years of experience with a strong focus on
-        creating accessible web solutions and engaging user experiences{" "}
+        <br></br>
+        In 2021 I took the chance and began studying multimedia design. This was
+        where I found my passion in UX/UI and surprisingly, Frontend Development
+        as well. Since then I have gained 2 years of experience with a strong
+        focus on creating accessible web solutions and engaging user experiences{" "}
       </p>
     </section>
   );
